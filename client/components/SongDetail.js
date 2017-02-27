301a12153835a98df9758230021492e88ac308fc
Roman Boboshko <romanboboshko89@gmail.com>
2017-02-28 01:35:50 +0200

    Update song detail and get data by id

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data;
+
+    if (!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
