7ac93c357426d104f89d138382d141e8bf44e557
Roman Boboshko <romanboboshko89@gmail.com>
2017-02-28 01:00:39 +0200

    Add song detail page and route, add single song query

--- client/index.js
+++ client/index.js
@@ -8,6 +8,7 @@ import './style/style.css';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -16,8 +17,9 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-          <IndexRoute component={SongList}/>
-          <Route path="songs/new" component={SongCreate}/>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
